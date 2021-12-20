dd6cba20091d1394e29d5d7c9d0ebffc84e36674
Dorkiliu <dorkiliu@tencent.com>
2021-12-20 20:18:31 +0800

    feat: add axios

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Text.scss';
+import axios from 'axios';
 
 function Text() {
+  const getMusic = async (name: string) => {
+    const res = await axios.get(`https://api.apiopen.top/searchMusic?name=${name}`)
+    console.log(res)
+  }
   return (
     <div className="text">
       <div className="purple-text">Text</div>
       <div className="orange-text">Text</div>
+      <button onClick={() => getMusic('黑色毛衣')}>get music</button>
     </div>
   );
 }
